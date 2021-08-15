e01b03b066927247d99f4c885ae8ce9a7af8deea
Rasaf-Ibrahim <rasaf.ibrahim25@gmail.com>
2021-08-15 17:52:52 +0600

    updated

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nuxt-tailwind-scss',
+    title: 'Aditto Nahian',
     htmlAttrs: {
       lang: 'en'
     },
