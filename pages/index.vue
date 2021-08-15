c616b94321441ec05bf1b8b60e9119e1becf5455
Rasaf-Ibrahim <rasaf.ibrahim25@gmail.com>
2021-08-15 15:37:18 +0600

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,86 @@
+<template>
+
+
+ <div id="body" class="font-sans antialiased h-screen text-gray-900 leading-normal tracking-wider bg-cover" style="background-image:url('https://source.unsplash.com/1L71sPT5XKc');">
+
+
+  <div class="max-w-4xl flex items-center  h-screen flex-wrap mx-auto my-32 lg:my-0">
+    
+	<!--Main Col-->
+	<div id="profile" class="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-75 mx-6 lg:mx-0">
+
+
+
+
+
+
+ </div>
+
+
+
+
+<!-- 
+	<script src="https://unpkg.com/popper.js@1/dist/umd/popper.min.js"></script>
+	<script src="https://unpkg.com/tippy.js@4"></script>
+	<script> -->
+		<!-- //Init tooltips
+		tippy('.link',{
+		  placement: 'bottom'
+		}) -->
+
+	
+
+
+</template>
+
+
+
+
+
+
+
+
+// <script>
+//     export default {
+
+
+//         mounted () {
+
+
+          
+
+// 	//Toggle mode
+// 		const toggle = document.querySelector('.js-change-theme');
+// 		const body = document.querySelector('#body');
+// 		const profile = document.getElementById('profile');
+		
+		
+// 		toggle.addEventListener('click', () => {
+
+// 		  if (body.classList.contains('text-gray-900')) {
+// 			toggle.innerHTML = "☀️";
+// 			body.classList.remove('text-gray-900');
+// 			body.classList.add('text-gray-100');
+// 			profile.classList.remove('bg-white');
+// 			profile.classList.add('bg-gray-900');
+// 		  } else
+// 		  {
+// 			toggle.innerHTML = "🌙";
+// 			body.classList.remove('text-gray-100');
+// 			body.classList.add('text-gray-900');
+// 			profile.classList.remove('bg-gray-900');			
+// 			profile.classList.add('bg-white');
+			
+// 		  }
+// 		});
+		
+   
+  
+
+
+//         },
+
+        
+//     }
+// </script>
+
